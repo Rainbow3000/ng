9ba7e7934b43edbb4c1048c7ff744ab3aa3af2e0
nguyenducthinh <nguyenducthinh0401@gmail.com>
2023-12-12 01:12:43 +0700

    goto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,59 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { TableListComponent } from './components/table-list/table-list.component';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    HeaderComponent,
+    HomeComponent,
+    SidebarComponent,
+    FilterComponent,
+    TableListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzGridModule,
+    NzMenuModule,
+    NzIconModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzSelectModule,
+    NzTableModule,
+    NzDividerModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/table-list/table-list.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+
+
+interface IEmployee{
+  id: number;
+  employeeName: string;
+  employeeCode: string;
+  position:string,
+  manager:string,
+  dob:string,
+  gender:string,
+  phoneNumber:string,
+  status:string,
+  email:string
+}
+
+
+@Component({
+  selector: 'app-table-list',
+  templateUrl: './table-list.component.html',
+  styleUrl: './table-list.component.scss'
+})
+export class TableListComponent implements OnInit {
+    ngOnInit(): void {
+      throw new Error('Method not implemented.');
+    }
+    listOfData:IEmployee[] = [
+      {
+        id:1,
+        employeeCode:"nv-1",
+        employeeName:"Nguyễn Đức Thịnh",
+        position:"Thực tập",
+        manager:"Nguyễn Văn Sơn",
+        dob:"22/12/2023",
+        gender:"Nam",
+        phoneNumber:"02385235928",
+        status:"Đang làm việc",
+        email:"nguyenducthinh0401@gmail.com"
+      }
+    ]
+
+    listOfSelection = [
+      {
+        text: 'Select All Row',
+        onSelect: () => {
+          this.onAllChecked(true);
+        }
+      },
+      {
+        text: 'Select Odd Row',
+        onSelect: () => {
+          this.listOfCurrentPageData.forEach((data, index) => this.updateCheckedSet(data.id, index % 2 !== 0));
+          this.refreshCheckedStatus();
+        }
+      },
+      {
+        text: 'Select Even Row',
+        onSelect: () => {
+          this.listOfCurrentPageData.forEach((data, index) => this.updateCheckedSet(data.id, index % 2 === 0));
+          this.refreshCheckedStatus();
+        }
+      }
+    ];
+    checked = false;
+    indeterminate = false;
+    listOfCurrentPageData: readonly IEmployee[] = [];
+    setOfCheckedId = new Set<number>();
+  
+    updateCheckedSet(id: number, checked: boolean): void {
+      if (checked) {
+        this.setOfCheckedId.add(id);
+      } else {
+        this.setOfCheckedId.delete(id);
+      }
+    }
+  
+    onItemChecked(id: number, checked: boolean): void {
+      this.updateCheckedSet(id, checked);
+      this.refreshCheckedStatus();
+    }
+  
+    onAllChecked(value: boolean): void {
+      this.listOfCurrentPageData.forEach(item => this.updateCheckedSet(item.id, value));
+      this.refreshCheckedStatus();
+    }
+  
+    onCurrentPageDataChange($event: readonly IEmployee[]): void {
+      this.listOfCurrentPageData = $event;
+      this.refreshCheckedStatus();
+    }
+  
+    refreshCheckedStatus(): void {
+      this.checked = this.listOfCurrentPageData.every(item => this.setOfCheckedId.has(item.id));
+      this.indeterminate = this.listOfCurrentPageData.some(item => this.setOfCheckedId.has(item.id)) && !this.checked;
+    }
+}
